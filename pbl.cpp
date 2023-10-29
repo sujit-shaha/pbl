591631872ce5dd19aada16da996eef243b0c99c2
sujit-shaha <sachinshaha222sss@gmail.com>
2023-10-29 23:47:12 +0530

    age facility by omkar and modified by me

--- pbl.cpp
+++ pbl.cpp
@@ -6,25 +6,33 @@
 using namespace std;
 class Donor {
 public:
-    Donor(const string& name, const string& bloodType,float &quantity)
-        : name(name), bloodType(bloodType), quantity(quantity) {}
-    virtual void display() const {
-        cout << "Name: " << name << " | Blood Type: " << bloodType << " | Total blood denoted in lifespan: "<<quantity<< endl;
+     Donor(const string &name, const string &bloodGroup, float &quantity, int &age)
+        : name(name), bloodGroup(bloodGroup), quantity(quantity), age(age) {}
+    virtual void display() const
+    {
+        cout << "Name: " << name << " | Age: " << age << " | Blood Group: " << bloodGroup << " | Total blood denoted in lifespan: " << quantity << endl;
     }
-    const string& getBloodType() const {
-        return bloodType;
+    const string &getbloodGroup() const
+    {
+        return bloodGroup;
     }
-    const string& getName() const {
+    const string &getName() const
+    {
         return name;
     }
-    float& getQuantity()
+    float &getQuantity()
     {
         return quantity;
     }
+    int getAge() const
+    {
+        return age;
+    }
     float quantity;
 private:
+    int age;
     string name;
-    string bloodType;
+    string bloodGroup;
 };
 class BloodBank {
 public:
@@ -56,8 +64,9 @@ public:
     while (getline(fin, line)) {
         string name, blood;
         float qty;
-        if (fin >> name >> blood >> qty) {
-           m[blood].push_back(name+" "+to_string(qty));
+        int age;
+        if (fin >> name >> blood >> qty >> age) {
+           m[blood].push_back("name : " + name+" Quantity : "+to_string(qty)+" Age : "+to_string(age));
         }
     }
     map<string,list<string>>::iterator i = m.begin();
@@ -132,12 +141,12 @@ public:
 		quantity+=qty;
 		removeDonor(donor.getName());
 		fout.open("bloodBankDonar.txt",ios::app);
-                fout<<donor.getName() << " "<<donor.getBloodType()<<" "<< quantity <<" "<<endl;
+                fout<<donor.getName() << " "<<donor.getbloodGroup()<<" "<< quantity <<" "<<donor.getAge()<<" "<<endl;
                 fout.close();
         count--;
     }
     int searchDonors(const string& bloodType) {
-        cout << "Donors with Blood Type " << bloodType << ":" << endl;
+        cout << "Donors with Blood Group " << bloodType << ":" << endl;
 //        for (const Donor& donor : donors) {
 //            if (donor.getBloodType() == bloodType) {
 //                donor.display();}}
@@ -154,9 +163,10 @@ public:
     while (getline(fin, line)) {
         string name, blood;
         float qty;
-        if (fin >> name >> blood >> qty) {
+        int age;
+        if (fin >> name >> blood >> qty>> age) {
             if (blood == bloodType) {
-                cout << "Name: " << name << ", Blood Type: " << blood << " Quantity : "<<qty<<endl;
+                cout << "Name: " << name << ", Blood Group: " << blood << " Quantity : "<<qty<<" Age : "<<age<<endl;
                 fin.close();
                 return 1;
                 
@@ -171,25 +181,92 @@ public:
 
 void removeDonor(string data){
 //	cout<<"Giving blood of donor : "<<data<<" : "<<endl;
-	ifstream infile("bloodBankDonar.txt",ios::app);
-	ofstream outfile("temp.txt");
-	infile.seekg(0,infile.beg);
-	if(!infile){
-		cerr << "Error opening file!" <<endl;
+//	ifstream infile("bloodBankDonar.txt",ios::app);
+//	ofstream outfile("temp.txt");
+//	infile.seekg(0,infile.beg);
+//	if(!infile){
+//		cerr << "Error opening file!" <<endl;
+//        return;
+//	}
+//	
+//	string line;
+//	while(getline(infile,line)){
+//		if(line.find(data)==string::npos){
+//			outfile<<line<<endl;
+//		}
+//	}
+//	infile.close();
+//	outfile.close();
+//	
+//	remove("bloodBankDonar.txt");
+//	rename("temp.txt","bloodBankDonar.txt");
+
+
+
+    ifstream infile("bloodBankDonar.txt");
+    ofstream outfile("temp.txt");
+    if (!infile.is_open()) {
+        cerr << "Error opening file!" << endl;
         return;
-	}
-	
-	string line;
-	while(getline(infile,line)){
-		if(line.find(data)==string::npos){
-			outfile<<line<<endl;
-		}
-	}
-	infile.close();
-	outfile.close();
-	
-	remove("bloodBankDonar.txt");
-	rename("temp.txt","bloodBankDonar.txt");
+    }
+
+    string line;
+    while (getline(infile, line)) {
+        if (line.find(data) == string::npos) {
+            outfile << line << endl;
+        }
+    }
+    infile.close();
+    outfile.close();
+
+    remove("bloodBankDonar.txt");
+    rename("temp.txt", "bloodBankDonar.txt");
+
+
+}
+
+void donateBlood(){
+	cout<<"Enter which blood group you want : ";
+	string bloodGroup;
+	cin>>bloodGroup;
+	cout<<"Enter the quantity of blood required : ";
+	float quant;
+	cin>>quant;
+	 ifstream fin;
+        fin.open("bloodBankDonar.txt",ios::app);
+    if (!fin.is_open()) {
+        cerr << "Error opening file!" <<endl;
+        return ;
+    }
+
+    string line;
+    fin.seekg(0,fin.beg);
+    while (getline(fin, line)) {
+        string name, blood;
+        float qty;
+        int age;
+        if (fin >> name >> blood >> qty>> age) {
+            if (blood == bloodGroup) {
+            	if(qty >= quant){
+            	removeDonor(name);
+				if((qty-quant)!=0)
+                {
+                	ofstream fout;
+                	fout.open("bloodBankDonar.txt",ios::app);
+                fout<<name << " "<<blood<<" "<< qty-quant <<" "<<age<<" "<<endl;
+                fout.close();
+				}
+				cout<<"Donar found"<<endl;
+                cout << "Name: " << name << ", Blood Group: " << blood << " Quantity : "<<qty<<" Age : "<<age<<endl;
+                fin.close();
+                return;
+            }
+            }
+        }
+    }
+    
+    cout<<"Data not found";
+    return ;
 }
 
 
@@ -219,12 +296,12 @@ int main() {
     ifstream fin;
     ofstream fout;
     fout.open("bloodBankDonar.txt",ios::app);
-    fout<<endl;
+//    fout<<endl;
     fout.close();
 
 
     while (true) {
-        cout<<endl;
+       A: cout<<endl;
         cout<<"***************************************"<<endl;
         cout << "Blood Bank Management System Menu:" << endl;
         cout << "1. Add Donor" << endl;
@@ -232,7 +309,7 @@ int main() {
         cout << "3. Display Notifications" << endl;
         cout << "4. Create Blood Request" << endl;
         cout << "5. Display list of donors"<<endl;
-        cout << "6. Remove Donor" << endl;
+        cout << "6. Give Blood" << endl;
         cout << "7. Exit"<<endl;
         cout << "Enter your choice: ";
         int choice;
@@ -240,16 +317,45 @@ int main() {
         cout<<endl;
         switch (choice) {
             case 1: {
-                string name, bloodType;
-                float qty;
-                int freq;
-                cout << "Enter donor's name: ";
-                cin >> name;
-                cout << "Enter donor's blood type: ";
-                cin >> bloodType;
-                cout<<"Enter donor's denoted blood quantity: ";
-                cin>>qty;
-                Donor donor(name, bloodType,qty);
+            string name, bloodGroup;
+            float qty;
+            int freq, age;
+            string greeting;
+            cout << "Enter donor's name: ";
+            cin >> name;
+            cout << "Enter donor's blood group: ";
+            cin >> bloodGroup;
+            cout << "Enter donor's age: ";
+            cin >> age;
+            try
+            {
+                string s1 = "We are really appreciate your willingness but you can't donate your blood since your are not 18 yrs yet.";
+                string s2 = "We are really appreciate your willingness but you can't donate your blood since your are aged person.";
+                if (age < 18)
+                    throw s1;
+                if (age > 65)
+                    throw s2;
+            }
+            catch (string s)
+            {
+                cout << s << endl;
+                goto A;
+            }
+            B:
+            cout << "Enter quantity of blood in litres, donor want to donate: ";
+            cin >> qty;
+            try
+            {
+                string s = "Thank you!!! but you can not donate more than 500 ml of blood.";
+                if (qty > 0.5)
+                    throw s;
+            }
+            catch (string s)
+            {
+                cout << s << endl;
+                goto B;
+            }
+            Donor donor(name, bloodGroup, qty, age);
                 bloodBank.addDonor(donor);
                 if(bloodBank.search(donor.getName())!=0)
                 {
@@ -260,7 +366,7 @@ int main() {
                 }
                 else{
                 fout.open("bloodBankDonar.txt",ios::app);
-                fout<<name << " "<<bloodType<<" "<< qty <<" "<<endl;
+                fout<<name << " "<<bloodGroup<<" "<< qty <<" "<<age<<endl;
                 fout.close();
                 cout << "Donor added successfully." << endl;
                 break;
@@ -289,10 +395,11 @@ int main() {
                 bloodBank.display();
                 break;
             case 6:{
-            	string bloodType;
-                cout << "Enter blood type to search: ";
-                cin >> bloodType;
-            	bloodBank.removeDonor(bloodType);
+//            	string bloodType;
+//                cout << "Enter blood type to search: ";
+//                cin >> bloodType;
+//            	bloodBank.removeDonor(bloodType);
+				bloodBank.donateBlood();
             	break;
             }
             case 7:
